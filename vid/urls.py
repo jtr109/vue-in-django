70529baccc6119ef3ecd8257d6370be39cbdbfb2
jtr109 <lyp_login@outlook.com>
2019-05-29 16:36:16 +0800

    refactor(vid): add api in url

--- vid/urls.py
+++ vid/urls.py
@@ -18,5 +18,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('account/', include('account.urls')),
+    path('api/account/', include('account.urls')),
 ]
