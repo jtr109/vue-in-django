9a845aee30a40e54c2cadd3e89241bdf854ef343
jtr109 <lyp_login@outlook.com>
2019-05-29 20:38:14 +0800

    feat(vid): edit user page msg

--- frontend/src/pages/user/app.vue
+++ frontend/src/pages/user/app.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="../../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="This Is The User Page"/>
   </div>
 </template>
 
