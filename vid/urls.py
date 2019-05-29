0ed4553eaee6606fa2c42a38954c07a6685d28db
jtr109 <lyp_login@outlook.com>
2019-05-29 20:50:08 +0800

    feat(vid): render frontend page

--- vid/urls.py
+++ vid/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # views
+    path('login/', TemplateView.as_view(template_name='login.html')),
+    # apis
     path('api/account/', include('account.urls')),
 ]
